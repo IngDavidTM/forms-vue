c6e04e61c173484b723c73ba74f875767819bdca
IngDavidTM <david-1011@hotmail.com>
2024-05-15 10:54:17 -0500

    Add v-model modifiers

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -2,11 +2,11 @@
   <form @submit.prevent="submitForm">
     <div class="form-control">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="userName" />
+      <input id="user-name" name="user-name" type="text" v-model.trim="userName" />
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <input id="age" name="age" type="number" v-model.number="userAge" />
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
@@ -57,12 +57,17 @@ export default {
   data() {
     return {
       userName: '',
+      userAge: null
     };
   },
   methods: {
     submitForm() {
       console.log('Username:' + this.userName);
       this.userName = '';
+      console.log('User Age:');
+      console.log(this.userAge);
+      console.log(30);
+      this.userAge = null;
     },
   },
 };
