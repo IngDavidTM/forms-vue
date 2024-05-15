b1233a8dcabbcb1e59a9cc169db49a5ab52a0767
IngDavidTM <david-1011@hotmail.com>
2024-05-15 11:12:13 -0500

    Use v-model for checkboxes and radio buttons

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -2,7 +2,12 @@
   <form @submit.prevent="submitForm">
     <div class="form-control">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model.trim="userName" />
+      <input
+        id="user-name"
+        name="user-name"
+        type="text"
+        v-model.trim="userName"
+      />
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -10,7 +15,7 @@
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="referrer">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -19,32 +24,54 @@
     <div class="form-control">
       <h2>What are you interested in?</h2>
       <div>
-        <input id="interest-news" name="interest" type="checkbox" />
+        <input
+          id="interest-news"
+          name="interest"
+          type="checkbox"
+          value="news"
+          v-model="interest"
+        />
         <label for="interest-news">News</label>
       </div>
       <div>
-        <input id="interest-tutorials" name="interest" type="checkbox" />
+        <input
+          id="interest-tutorials"
+          name="interest"
+          type="checkbox"
+          value="tutorials"
+          v-model="interest"
+        />
         <label for="interest-tutorials">Tutorials</label>
       </div>
       <div>
-        <input id="interest-nothing" name="interest" type="checkbox" />
+        <input
+          id="interest-nothing"
+          name="interest"
+          type="checkbox"
+          value="nothing"
+          v-model="interest"
+        />
         <label for="interest-nothing">Nothing</label>
       </div>
     </div>
     <div class="form-control">
       <h2>How do you learn?</h2>
       <div>
-        <input id="how-video" name="how" type="radio" />
+        <input id="how-video" name="how" type="radio" value="video" v-model="how" />
         <label for="how-video">Video Courses</label>
       </div>
       <div>
-        <input id="how-blogs" name="how" type="radio" />
+        <input id="how-blogs" name="how" type="radio" value="blogs" v-model="how" />
         <label for="how-blogs">Blogs</label>
       </div>
       <div>
-        <input id="how-other" name="how" type="radio" />
+        <input id="how-other" name="how" type="radio" value="other" v-model="how" />
         <label for="how-other">Other</label>
       </div>
+    </div>
+    <div class="form-control">
+      <input type="checkbox" id="confirm-terms" name="confirm-terms" v-model="confirm" />
+      <label for="confirm-terms">Agree to terms of use?</label>
     </div>
     <div>
       <button>Save Data</button>
@@ -57,7 +84,11 @@ export default {
   data() {
     return {
       userName: '',
-      userAge: null
+      userAge: null,
+      referrer: 'wom',
+      interest: [],
+      how: null,
+      confirm: false
     };
   },
   methods: {
@@ -66,8 +97,17 @@ export default {
       this.userName = '';
       console.log('User Age:');
       console.log(this.userAge);
-      console.log(30);
       this.userAge = null;
+      console.log('Referrer:' + this.referrer);
+      this.referrer = 'wom';
+      console.log('Checkboxes:');
+      console.log(this.interest);
+      this.interest = [];
+      console.log('Radio buttons:');
+      console.log(this.how);
+      this.how = null;
+      console.log('Terms confirmed:' + this.confirm);
+      this.confirm = false;
     },
   },
 };
